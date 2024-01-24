018087338f529e5913fa068ce335b4fac16b3dbe
Chris <104800006+renners7777@users.noreply.github.com>
2024-01-24 20:06:16 +0000

    Added function to display more articles on page in main.js and updated index.html

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-import journalData  from "./JournalData";
+function displayArticles() {
+    let hiddenArticles = document.getElementById("hidden-articles");
+    let elBtn = document.querySelector(".btn")
+    if (hiddenArticles.style.display === "none") {
+    hiddenArticles.style.display = "block";
+    elBtn.textContent = "View less"
+    } else {
+    hiddenArticles.style.display = "none";
+    elBtn.textContent = "View more"
+    }
+}
